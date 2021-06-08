37f49823c767f3dd294f0860da94227bc72d4a04
Erroltakiya13 <>
2021-06-08 16:23:34 +0000

    j'ai ajouté le code

--- src/SeriEatHub.java
+++ src/SeriEatHub.java
@@ -8,12 +8,19 @@ import java.util.Scanner;
 */
 public class SeriEatHub {
 
-	public static void chargerFichier(String nomFichier, ModelEatHub model) {
+	public static void chargerFichier(String nomFichier, ModelEatHub model) {  
 	    try {
 	        File myObj = new File(nomFichier);
 	        Scanner myReader = new Scanner(myObj);
-	                
-	          //ajoute le 1er client
+          while (i=0; i<20; i++) {
+            //ajoute le 1er client
+	          String Tel = myReader.nextLine();
+	          String Nom = myReader.nextLine();
+	          String adresse = myReader.nextLine();
+	          model.ajouterClient(Tel, Nom, adresse);
+          }
+	          /*
+            //ajoute le 1er client
 	          String Tel = myReader.nextLine();
 	          String Nom = myReader.nextLine();
 	          String adresse = myReader.nextLine();
@@ -24,7 +31,7 @@ public class SeriEatHub {
 	          Nom = myReader.nextLine();
 	          adresse = myReader.nextLine();
 	          model.ajouterClient(Tel, Nom, adresse);
-	          
+	        */
 	        myReader.close();
 	      } catch (Exception e) {
 	        System.out.println("An error occurred.");
